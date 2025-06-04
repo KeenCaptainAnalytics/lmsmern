964dc594a340c3a68b6c1b2ccdfe2d0040b08226
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-06-04 20:30:46 +0530

    Update object2.js

--- javascript/object2.js
+++ javascript/object2.js
@@ -1,34 +1,54 @@
 
-class Person{
-    constructor(age, name, phone){
-        this.name = name
-        this.age = age
-        this.phone = phone
-    }
-
-    getInfo() {
-        console.log(this.name+ "  "+ this.age+"  "+this.phone)
-    }
-
-    getName = ()=>{
-        console.log(this.name)
-    }
-
+let poojaObj = {
+    name :"pooja",
+    money:10000
 }
 
-class Student extends Person{
-    constructor(name , age, phone, marks){
-        super(age, name, phone);
-        this.marks = marks
-        // this.age = age
-        // this.name = name
-        // this.phone = phone
+let obj2 = Object.create(poojaObj);
+obj2.name = "tina"
+obj2.account = 7
+// console.log(obj2.money)
 
-    }
-}
 
-let obj1 = new Student("tina", 67, 986645, 890);
-console.log(obj1)
+let obj3 = Object.create(obj2)
+obj3.name ="rahul"
+
+console.log(obj3.money)
+
+
+
+
+
+// class Person{
+//     constructor(age, name, phone){
+//         this.name = name
+//         this.age = age
+//         this.phone = phone
+//     }
+
+//     getInfo() {
+//         console.log(this.name+ "  "+ this.age+"  "+this.phone)
+//     }
+
+//     getName = ()=>{
+//         console.log(this.name)
+//     }
+
+// }
+
+// class Student extends Person{
+//     constructor(name , age, phone, marks){
+//         super(age, name, phone);
+//         this.marks = marks
+//         // this.age = age
+//         // this.name = name
+//         // this.phone = phone
+
+//     }
+// }
+
+// let obj1 = new Student("tina", 67, 986645, 890);
+// console.log(obj1)
 
 
 
